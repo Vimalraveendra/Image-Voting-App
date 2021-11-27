52be53d7401a5244d1e790779e76cc3f15de77c4
Vimal Raveendran <vimalraveendra@gmail.com>
2021-11-27 20:05:20 +0100

    Deleted the count state from the Image component & updated the count based on the click

--- src/components/ImageDisplay/Image.jsx
+++ src/components/ImageDisplay/Image.jsx
@@ -1,18 +1,18 @@
 import "./image.css"
-import {useState}  from 'react'
+
 
 const Image =({id,title,image})=>{
-   const[count ,setCount] =useState(0)
+  
 
    const handleIncrement=(e)=>{
-       console.log("hello",e.target)
+       e.target.textContent++;
    }
     return(
         <div className="image">
          <img src={image} alt={title} />
          <div className="image-footer">
          <span className="thumb"><i className="fas fa-thumbs-up"></i></span>
-         <span id={id} onClick={handleIncrement}>{count}</span>
+         <span id={id} onClick={handleIncrement}>0</span>
          </div>
         </div>
     )
